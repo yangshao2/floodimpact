61a20bd8e331dd6b9fdf40dc7be3e0406c714f41
Yang Shao <yshao@n2d037fca.cns.vt.edu>
2024-02-13 18:51:18 -0500

    obtain census data

--- get_censusdata.R
+++ get_censusdata.R
@@ -8,16 +8,22 @@ library(viridis)
 library(sf)
 #library(mapview)
 
-fips<-read.csv('fips.csv')
+fips<-read.csv('https://github.com/yangshao2/floodimpact/raw/main/fips.csv')
 
-#Enter the access key for census data;
+#Enter the access key for census data, replace **
 #A key can be acquired at http://api.census.gov/data/key_signup.html
 census_api_key("**",overwrite = TRUE,install=TRUE)
 
+#P1_001N !!Total Population
+#P2_002N    Hispanic
+#P2_005N    !!Total:!!Not Hispanic or Latino:!!Population of one race:!!White alone
+#P2_006N   !!Total:!!Not Hispanic or Latino:!!Population of one race:!!Black or African American
+#P2_008N !!Total:!!Not Hispanic or Latino:!!Population of one race:!!Asian alone
 
-for (i in 1:51){
+
+for (i in 1:nrow(fips)){
  fcode<- sprintf("%02d", fips$fips[i])
-l2020 <- get_decennial(geography = "tract", variables = 'P2_008N', year = 2020,
+l2020 <- get_decennial(geography = "tract", variables = 'P1_001N', year = 2020,
                   state=fcode,geometry = TRUE)
 outname<-paste0('b',fcode,'.shp')
 st_write(l2020,outname)
